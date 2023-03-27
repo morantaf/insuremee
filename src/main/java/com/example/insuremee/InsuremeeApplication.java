c8705096eda4b278acab41ea5e3db6eb65ffbbdf
morantafall <fall_moranta@live.fr>
2023-03-27 09:01:21 +0200

    test push

--- src/main/java/com/example/insuremee/InsuremeeApplication.java
+++ src/main/java/com/example/insuremee/InsuremeeApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class InsuremeeApplication {
 
+	//making some change to test
 	public static void main(String[] args) {
 		SpringApplication.run(InsuremeeApplication.class, args);
 	}
